d2a83e460db795870012e62696435629630b13f7
Samrel <samsigelr556@gmail.com>
2024-06-25 11:25:52 -0600

    Hostile Neural Networks gregified
    Gregification of HNN and data module clean up completed

---
+++ kubejs/startup_scripts/hostile_neural_networks.js
@@ -0,0 +1,7 @@
+// Creates custom Generalized Predictions for Hostile Neural Networks
+
+StartupEvents.registry('item', event => {
+    event.create('aether_prediction').texture('samrelpack:item/hostilenetworks/aether_prediction').displayName('Generalized Aether Prediction')
+    event.create('blue_skies_prediction').texture('samrelpack:item/hostilenetworks/blue_skies_prediction').displayName('Generalized Blue Skies Prediction')
+    event.create('undergarden_prediction').texture('samrelpack:item/hostilenetworks/undergarden_prediction').displayName('Generalized Undergarden Prediction')
+})
